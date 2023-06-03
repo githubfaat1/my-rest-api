c6c45e67ff325b475b38e36a29cdc17b495bbd9f
muhammad syafaat <135451675+githubfaat1@users.noreply.github.com>
2023-06-04 03:54:57 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "mongodb+srv://akunfaat1:EAVbEudomKVG7iFl@cluster0.s5rehpu.mongodb.net/" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://akunfaat1:EAVbEudomKVG7iFl@cluster0.s5rehpu.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "akunfaat1@gmail.com" //email
 global.email_password = "bzuuldwsmesintkj" //application password email
